4594fd1e9ab65b3e341897152fe36f90e0cc19f3
ritikasakpal <37076740+ritikasakpal@users.noreply.github.com>
2019-10-25 14:00:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.get('/', (req, res) => {
 
 app.get('/data/esp8266', (req, res) => {
   const db = fire.firestore();
-    db.settings({
-      timestampsInSnapshots: true
-    });
+//     db.settings({
+//       timestampsInSnapshots: true
+//     });
     var wholeData = []
 	db.collection('lin_esp8266').orderBy('waktu', 'asc').get()
     .then(snapshot => {
